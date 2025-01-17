0d112105dd4f0c03cf87ae173a3504c4a41046b6
Daniil Polyanskiy <145911801+k1nglyy@users.noreply.github.com>
2025-01-17 12:11:39 +0300

    kajsdhadjk

--- database/__init__.py
+++ database/__init__.py
@@ -2,5 +2,5 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from .database import *
 
-engine = create_async_engine('postgresql+asyncpg://postgres:postgres@localhost/asyncalchemy')
+engine = create_async_engine('postgresql+asyncpg://admin:1111@localhost/smartfridge')
 sessions = async_sessionmaker(engine)
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from contextlib import asynccontextmanager
 
 import database
 import routes
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Creating tables in database")
+    async with database.engine.begin() as connection:
+        await connection.run_sync(database.Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(routes.router)
 
 
 @app.get("/", response_class=PlainTextResponse)
 def read_root():
     return "йоу сасло?"
-
-
