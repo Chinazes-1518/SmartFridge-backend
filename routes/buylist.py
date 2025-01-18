7465a5d963cf455eb102778b1a5b90d1068ac011
JaanDev <67502867+JaanDev@users.noreply.github.com>
2025-01-19 00:09:44 +0300

    token verification

--- routes/buylist.py
+++ routes/buylist.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Header
 from sqlalchemy import select, insert, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import BaseModel
+from typing import Annotated
 
 import database
 import utils
@@ -10,8 +11,9 @@ router = APIRouter(prefix='/buylist')
 
 
 @router.get('/get')
-async def get_buylist() -> str:
+async def get_buylist(token: Annotated[str, Header()]) -> str:
     async with database.sessions.begin() as session:
+        await utils.verify_token(session, token)
         result = await session.execute(select(database.BuyList))
         buylist = result.scalars().all()
         return utils.json_responce({"buylist": buylist})
@@ -23,8 +25,9 @@ class BuylistAdd(BaseModel):
     
 
 @router.post('/add')
-async def add_to_buylist(data: BuylistAdd) -> str:
+async def add_to_buylist(data: BuylistAdd, token: Annotated[str, Header()]) -> str:
     async with database.sessions.begin() as session:
+        await utils.verify_token(session, token)
         existing_item = await session.execute(select(database.BuyList).where(database.BuyList.prod_type_id == data.prod_type_id))
         if existing_item.scalar_one_or_none():
             raise HTTPException(400, '{"error": "Продукт уже есть в списке покупок"}')
@@ -34,8 +37,9 @@ async def add_to_buylist(data: BuylistAdd) -> str:
 
 
 @router.delete('/remove/{buylist_id}')
-async def remove_from_buylist(buylist_id: int) -> str:
+async def remove_from_buylist(buylist_id: int, token: Annotated[str, Header()]) -> str:
     async with database.sessions.begin() as session:
+        await utils.verify_token(session, token)
         existing_item = await session.execute(select(database.BuyList).where(database.BuyList.id == buylist_id))
         if not existing_item.scalar_one_or_none():
             raise HTTPException(404, '{"error": "Продукт не найден в списке покупок"}')
--- utils.py
+++ utils.py
@@ -2,6 +2,16 @@ import hashlib
 from uuid import uuid4
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi import HTTPException
+from sqlalchemy import select
+
+import database
+
+
+async def verify_token(session, token):
+    existing_item = await session.execute(select(database.Users).where(database.Users.token == token.strip()))
+    if existing_item.scalar_one_or_none() is None:
+        raise HTTPException(403, '{"error": "Авторизация не удалась"}')
 
 
 def json_responce(data: dict) -> JSONResponse:
