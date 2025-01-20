1dfeac388fa32e2973ed0c8a7ff3f23efc913ecc
JaanDev <67502867+JaanDev@users.noreply.github.com>
2025-01-20 16:46:08 +0300

    saslo chupep8

--- routes/products.py
+++ routes/products.py
@@ -17,9 +17,9 @@ async def get_all(token: Annotated[str, Header()]) -> JSONResponse:
 
         stmt = text("""
         SELECT products.id AS prod_id, products.production_date, products.expiry_date,
-        product_types.name AS type_name, product_types.amount, product_types.units,
+        product_types.name AS type_name, product_types.amount, product_types.units, product_types.id AS type_id,
         product_types.nutritional, product_types.measure_type, product_types.allergens,
-        product_categories.name AS cat_name
+        product_categories.name AS cat_name, product_categories.id AS cat_id
         FROM products
         JOIN product_types ON products.type_id = product_types.id
         JOIN product_categories ON product_types.category_id = product_categories.id
@@ -40,9 +40,9 @@ async def get_product(token: Annotated[str, Header()], id: int) -> JSONResponse:
 
         stmt = text("""
         SELECT products.id AS prod_id, products.production_date, products.expiry_date,
-        product_types.name AS type_name, product_types.amount, product_types.units,
+        product_types.name AS type_name, product_types.amount, product_types.units, product_types.id AS type_id,
         product_types.nutritional, product_types.measure_type, product_types.allergens,
-        product_categories.name AS cat_name
+        product_categories.name AS cat_name, product_categories.id AS cat_id
         FROM products
         JOIN product_types ON products.type_id = product_types.id
         JOIN product_categories ON product_types.category_id = product_categories.id
@@ -58,3 +58,16 @@ async def get_product(token: Annotated[str, Header()], id: int) -> JSONResponse:
             raise HTTPException(404, {'error': 'Продукт с этим id не найден'})
 
         return utils.json_responce(data)
+
+
+@router.get('/categories')
+async def get_cats(token: Annotated[str, Header()]) -> JSONResponse:
+    async with database.sessions.begin() as session:
+        await utils.verify_token(session, token)
+
+        req = await session.execute(select(database.ProductCategories))
+        data = req.scalars()
+
+        # print(data)
+
+        return utils.json_responce([{'id': x.id, 'name': x.name} for x in data])
