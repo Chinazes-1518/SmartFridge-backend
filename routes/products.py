efaf912f18aa25f0e45f2df7c2b8bb471fd35db1
JaanDev <67502867+JaanDev@users.noreply.github.com>
2025-01-20 16:35:21 +0300

    get products

--- routes/products.py
+++ routes/products.py
@@ -10,7 +10,7 @@ import utils
 router = APIRouter(prefix='/products')
 
 
-@router.get('/get_all')
+@router.get('/all')
 async def get_all(token: Annotated[str, Header()]) -> JSONResponse:
     async with database.sessions.begin() as session:
         await utils.verify_token(session, token)
@@ -20,18 +20,41 @@ async def get_all(token: Annotated[str, Header()]) -> JSONResponse:
         product_types.name AS type_name, product_types.amount, product_types.units,
         product_types.nutritional, product_types.measure_type, product_types.allergens,
         product_categories.name AS cat_name
+        FROM products
         JOIN product_types ON products.type_id = product_types.id
         JOIN product_categories ON product_types.category_id = product_categories.id
-        WHERE products.id = 3
-        """).columns(
-            database.Products.id, database.Products.production_date, database.Products.expiry_date,
-            database.ProductTypes.name, database.ProductTypes.amount, database.ProductTypes.units,
-            database.ProductTypes.nutritional, database.ProductTypes.measure_type, database.ProductTypes.allergens,
-            database.ProductCategories.name
-        )
+        """)
 
-        data = await session.execute(select(database.Products))
+        req = await session.execute(stmt)
+        data = req.mappings().all()
 
-        print(data.fetchone())
-        # for pr in data.scalars().fetchall():
-        #     print(pr)
+        # print(data)
+
+        return utils.json_responce(data)
+
+
+@router.get('/product')
+async def get_product(token: Annotated[str, Header()], id: int) -> JSONResponse:
+    async with database.sessions.begin() as session:
+        await utils.verify_token(session, token)
+
+        stmt = text("""
+        SELECT products.id AS prod_id, products.production_date, products.expiry_date,
+        product_types.name AS type_name, product_types.amount, product_types.units,
+        product_types.nutritional, product_types.measure_type, product_types.allergens,
+        product_categories.name AS cat_name
+        FROM products
+        JOIN product_types ON products.type_id = product_types.id
+        JOIN product_categories ON product_types.category_id = product_categories.id
+        WHERE products.id = :prod_id
+        """)
+
+        req = await session.execute(stmt, {'prod_id': id})
+        data = req.mappings().first()
+
+        # print(data)
+
+        if data is None:
+            raise HTTPException(404, {'error': 'Продукт с этим id не найден'})
+
+        return utils.json_responce(data)
