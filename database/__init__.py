42bb953a8b8e56317d2b30f57003d38a072cdccf
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2025-01-26 14:56:31 +0300

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -2,8 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from .database import *
 
-engine = create_async_engine('postgresql+asyncpg://root:1111@localhost:5432/smartfridge')
-# engine = create_async_engine('postgresql://neondb_owner:npg_UrhxVXHGm3t9@ep-super-recipe-a873bhyv-pooler.eastus2.azure.neon.tech/neondb?sslmode=require')
+# engine = create_async_engine('postgresql+asyncpg://root:1111@localhost:5432/smartfridge')
+engine = create_async_engine('postgresql+asyncpg://neondb_owner:npg_UrhxVXHGm3t9@ep-super-recipe-a873bhyv-pooler.eastus2.azure.neon.tech/neondb?sslmode=require')
 sessions = async_sessionmaker(engine)
 
 # ./postgres -D 'C:\Users\ученик.313-17\OneDrive\Документы\chupep8\db' -p 5432
