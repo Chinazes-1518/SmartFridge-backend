11e43454f07816556be3ec9cecb9ba881b809b47
JaanDev <67502867+JaanDev@users.noreply.github.com>
2025-01-30 23:00:38 +0300

    env

--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from dotenv import load_dotenv
+import os
 
 from .database import *
 
-engine = create_async_engine('postgresql+asyncpg://root:1111@localhost:5432/smartfridge')
-# engine = create_async_engine('postgresql+asyncpg://neondb_owner:npg_UrhxVXHGm3t9@ep-super-recipe-a873bhyv-pooler.eastus2.azure.neon.tech/neondb')
-sessions = async_sessionmaker(engine)
+load_dotenv()
 
-# ./postgres -D 'C:\Users\ученик.313-17\OneDrive\Документы\chupep8\db' -p 5432
+engine = create_async_engine(f'postgresql+asyncpg://root:{os.getenv("DB_PASSWORD")}@localhost:5432/smartfridge')
+sessions = async_sessionmaker(engine)
