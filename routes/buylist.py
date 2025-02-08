0ac1b89325f21db3afc71e2c7adebf7f0d6e6a53
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2025-02-08 22:02:53 +0300

    Update buylist.py

--- routes/buylist.py
+++ routes/buylist.py
@@ -41,7 +41,7 @@ async def add_to_buylist(data: BuylistAdd, token: Annotated[str, Header()]) -> J
         res = existing_item.scalar_one_or_none()
 
         if res is not None:
-            res.count += 1
+            res.count += data.count
             await session.commit()
         else:
             await session.execute(insert(database.BuyList).values(prod_type_id=data.prod_type_id, count=1))
